f76ca3d03d7c05f5b09896d1925715ab5c4c378d
Oscar Nunez <requinto31@gmail.com>
2023-02-04 15:50:45 -0500

    create question model

---
+++ models/question.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+  content: String,
+  commenter: { type: Schema.Types.ObjectId, ref: 'Profile'},
+  likes: { type: Array, default:[] }
+})
+
+const questionSchema = new Schema({
+  name: { type:String, required: true },
+  answered: Boolean,
+  comments: [commentSchema],
+  owner: { type: Schema.Types.ObjectId, ref: 'Profile' }
+},{
+  timestamps: true,
+})
+
+const Question = mongoose.model('Question', questionSchema)
+
+export { Question }
